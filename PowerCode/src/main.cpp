5b05e8127197b14fb5a5304eaefe3d4c6af69b18
Dipesh Manandhar <dipesh_manz@yahoo.com>
2020-05-24 18:53:35 -0400

    added code to test CAN within one MCU

--- PowerCode/src/main.cpp
+++ PowerCode/src/main.cpp
@@ -1,35 +1,44 @@
 #include <mbed.h>
 
-//Serial pc(USBTX, USBRX);
+
+#if defined (DEVICE_CAN) || defined(DOXYGEN_ONLY)
+
+Ticker ticker;
+DigitalOut led1(LED1);
+DigitalOut led2(LED2);
+/** The constructor takes in RX, and TX pin respectively.
+  * These pins, for this example, are defined in mbed_app.json
+  */
+CAN can1(PD_0, PD_1);
+CAN can2(PB_12, PB_13);
+char counter = 0;
+
+void send() {
+    printf("send()\n");
+    if(can1.write(CANMessage(1337, &counter, 1))) {
+        printf("wloop()\n");
+        counter++;
+        printf("Message sent: %d\n", counter);
+    } 
+    led1 = !led1;
+}
 
 int main() {
-
-  // put your setup code here, to run once:
-  DigitalOut led1(LED1);
-  //if(led1.is_connected())
-  //  pc.printf("led1 is initialized and connected!\n\r");
-
-  AnalogIn mypin(A0);
-  AnalogOut outpin(A5);
-
-  while(1) {
-    // put your main code here, to run repeatedly:
-    if(mypin > 0.3f)
-      led1 = 0;
-    else
-      led1 = 1;
-    outpin = mypin*0.7f;
-    wait_us(1000);
-
-
-    /*
-    led1 = 1;          // set LED1 pin to high
-    pc.printf("led1 = %d \n\r", (uint8_t)led1 );
-    wait(0.5);
-    
-    led1.write(0);     // set LED1 pin to low
-    pc.printf("led1 = %d \n\r",led1.read() );
-    wait(0.5);
-    */
-  }
+    printf("main()\n");
+    ticker.attach(&send, 1); // in seconds (float)
+    CANMessage msg;
+    while(1) {
+        printf("loop()\n");
+        if(can2.read(msg)) {
+            printf("Message received: %d\n", msg.data[0]);
+            led2 = !led2;
+        } 
+        //wait(0.2);
+        thread_sleep_for(200); // in ms
+    }
 }
+
+#else
+  #error CAN NOT SUPPORTED
+  
+#endif
